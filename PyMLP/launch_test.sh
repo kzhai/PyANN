b14bf504a485f2ffd42ddb4a6d2194189d4e0c27
Ke Zhai <kzhai@gpu5.corp.gq1.yahoo.com>
2015-12-10 06:15:49 +0000

    update

--- PyMLP/launch_test.sh
+++ PyMLP/launch_test.sh
@@ -2,22 +2,34 @@
 
 PYANN_HOME=$HOME/Workspace/PyANN
 
-if [ $# != 2 ]; then
-    echo "usage: launch_test.sh INPUT_DIRECTORY OUTPUT_DIRECTORY"
+if [ $# == 2 ]; then
+    INPUT_DIRECTORY=$1
+    OUTPUT_DIRECTORY=$2
+    BATCH_SIZE=100
+    GPU_DEVICE=gpu
+elif [ $# == 3 ]; then
+    INPUT_DIRECTORY=$1
+    OUTPUT_DIRECTORY=$2
+    BATCH_SIZE=$3
+    GPU_DEVICE=gpu
+elif [ $# == 4 ]; then
+    INPUT_DIRECTORY=$1
+    OUTPUT_DIRECTORY=$2
+    BATCH_SIZE=$3
+    GPU_DEVICE=$4
+else
+    echo "usage: launch_test.sh INPUT_DIRECTORY OUTPUT_DIRECTORY [BATCH_SIZE] [GPU_DEVICE]"
     exit
 fi
 
-INPUT_DIRECTORY=$(grealpath $1)
-OUTPUT_DIRECTORY=$(grealpath $2)
-
 for MODEL_DIRECTORY in $OUTPUT_DIRECTORY/*
 do
-	if [ -f "$MODEL_DIRECTORY" ]; then
-		continue
+    if [ -f "$MODEL_DIRECTORY" ]; then
+	continue
     fi
 	
-	if [ -f "$MODEL_DIRECTORY/model.pkl" ]; then
-		echo "Evaluating $MODEL_DIRECTORY"
-    	python -um PyMLP.launch_test --input_directory=$INPUT_DIRECTORY --model_directory=$MODEL_DIRECTORY --best_model_only
-	fi
+    if [ -f "$MODEL_DIRECTORY/model.pkl" ]; then
+	echo "Evaluating $MODEL_DIRECTORY"
+    	THEANO_FLAGS=mode=FAST_RUN,device=$GPU_DEVICE,floatX=float32 python -um PyMLP.launch_test --input_directory=$INPUT_DIRECTORY --model_directory=$MODEL_DIRECTORY --batch_size=$BATCH_SIZE --best_model_only
+    fi
 done
