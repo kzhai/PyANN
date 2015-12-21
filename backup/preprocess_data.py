3746b80716fa6480759f5403cd87f2e16f67cb78
Ke Zhai <kzhai@circlecar-lm.corp.yahoo.com>
2015-12-21 15:59:19 -0800

    update

--- backup/preprocess_data.py
+++ backup/preprocess_data.py
@@ -16,24 +16,10 @@ import theano.tensor
 import matplotlib
 import matplotlib.pyplot
 
-'''
-def preprocess_file(input_file, output_file, mean=None, std=None):
-    #input_file = os.path.join(input_directory, "test.feature.npy")
-    #output_file = os.path.join(output_directory, "test.feature.npy")
-    
-    input_data = scipy.load(input_file);
-    # input_data = input_data / numpy.float32(256);
-    
-    input_data = input_data / numpy.float32(1.0);
-    input_data -= numpy.mean(input_data);
-    input_data /= numpy.std(input_data);
-    
-    print input_file
-    print output_file
-    print input_data.dtype, numpy.max(input_data), numpy.min(input_data);
-    
-    numpy.save(output_file, input_data)
-'''
+def standard_normalization(input_data, data_mean, data_std):
+    input_data -= data_mean
+    input_data /= data_std
+    return input_data
 
 def preprocess_directory(input_directory, output_directory):
     input_train_file = os.path.join(input_directory, "train.feature.npy")
@@ -43,8 +29,12 @@ def preprocess_directory(input_directory, output_directory):
     # input_train_data = input_train_data / numpy.float32(256);
     
     input_train_data = input_train_data / numpy.float32(256.0);
-    train_mean = numpy.mean(input_train_data, axis=0)[numpy.newaxis, :]
-    input_train_data -= train_mean
+    # train_mean = numpy.mean(input_train_data, axis=0)[numpy.newaxis, :];
+    
+    train_mean = numpy.mean(input_train_data);
+    train_std = numpy.std(input_train_data);
+    
+    input_train_data = standard_normalization(input_train_data, train_mean, train_std)
     
     print input_train_data.dtype, input_train_data.shape, numpy.max(input_train_data), numpy.min(input_train_data);
     
@@ -56,8 +46,8 @@ def preprocess_directory(input_directory, output_directory):
     input_test_data = scipy.load(input_test_file);
     
     input_test_data = input_test_data / numpy.float32(256.0);
-    train_mean = numpy.mean(input_test_data, axis=0)[numpy.newaxis, :]
-    input_test_data -= train_mean
+    
+    input_test_data = standard_normalization(input_test_data, train_mean, train_std)
     
     print input_test_data.dtype, input_test_data.shape, numpy.max(input_test_data), numpy.min(input_test_data);
     
