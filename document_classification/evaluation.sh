d8ad2d686238ec6acf0e3991502c60d3cf0d17ba
Ke Zhai <kzhai@circlecar-lm.corp.yahoo.com>
2016-02-26 00:34:32 -0800

    update

--- document_classification/evaluation.sh
+++ document_classification/evaluation.sh
@@ -6,12 +6,20 @@ INPUT_DIRECTORY=$1
 echo "python document_classification/cosim.py $INPUT_DIRECTORY\\_normalized"
 
 python document_classification/cosim.py $INPUT_DIRECTORY\_normalized
+wait
 python document_classification/knn.py $INPUT_DIRECTORY\_normalized
+wait
 python document_classification/svm.py $INPUT_DIRECTORY\_normalized
+wait
 
 python document_classification/nb.py $INPUT_DIRECTORY
+wait
 python document_classification/lda.py $INPUT_DIRECTORY
+wait
 
 python document_classification/cosim.py $INPUT_DIRECTORY
+wait
 python document_classification/knn.py $INPUT_DIRECTORY
+wait
 python document_classification/svm.py $INPUT_DIRECTORY
+wait
