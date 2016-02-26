dff203c7cc08a6ac1f0d1b9d26d5b6329ec18a26
Ke Zhai <kzhai@circlecar-lm.corp.yahoo.com>
2016-02-26 00:24:12 -0800

    update

--- document_classification/evaluation.sh
+++ document_classification/evaluation.sh
@@ -4,15 +4,21 @@ PYANN_HOME=$HOME/Workspace/PyANN
 
 INPUT_DIRECTORY=$1
 
-if [ -d "$INPUT_DIRECTORY_normalized" ]; then
-	python document_classification/cosim.py $INPUT_DIRECTORY_normalized
-	python document_classification/knn.py $INPUT_DIRECTORY_normalized
-	python document_classification/svm.py $INPUT_DIRECTORY_normalized
+python document_classification/cosim.py $INPUT_DIRECTORY_normalized
+wait
+python document_classification/knn.py $INPUT_DIRECTORY_normalized
+wait
+python document_classification/svm.py $INPUT_DIRECTORY_normalized
+wait
 
-	python document_classification/nb.py $INPUT_DIRECTORY
-	python document_classification/lda $INPUT_DIRECTORY
-	
-	python document_classification/cosim.py $INPUT_DIRECTORY
-	python document_classification/knn.py $INPUT_DIRECTORY
-	python document_classification/svm.py $INPUT_DIRECTORY
-fi
+python document_classification/nb.py $INPUT_DIRECTORY
+wait
+python document_classification/lda $INPUT_DIRECTORY
+wait
+
+python document_classification/cosim.py $INPUT_DIRECTORY
+wait
+python document_classification/knn.py $INPUT_DIRECTORY
+wait
+python document_classification/svm.py $INPUT_DIRECTORY
+wait
