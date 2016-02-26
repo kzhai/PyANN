632b1bf461039a1285e6c689936c97799d7947a3
Ke Zhai <kzhai@circlecar-lm.corp.yahoo.com>
2016-02-25 22:47:45 -0800

    update

--- document_classification/svm.py
+++ document_classification/svm.py
@@ -9,20 +9,20 @@ import argparse
 import sklearn
 import sklearn.svm
 
-def main(data_directory, kernel="linear"):
+def main(data_directory):
     train_set_x = numpy.load(os.path.join(data_directory, "train.feature.npy"))
     train_set_y = numpy.load(os.path.join(data_directory, "train.label.npy"))
     
     test_set_x = numpy.load(os.path.join(data_directory, "test.feature.npy"))
     test_set_y = numpy.load(os.path.join(data_directory, "test.label.npy"))
     
-    clf = sklearn.svm.SVC(kernel=kernel)
+    clf = sklearn.svm.SVC(kernel="linear")
     clf.fit(train_set_x, train_set_y)
-    print "%s" % kernel, clf.score(test_set_x, test_set_y)
+    print "SVC+linear", clf.score(test_set_x, test_set_y)
     
-    clf = sklearn.svm.SVC(kernel=kernel)
+    clf = sklearn.svm.SVC(kernel="rbf")
     clf.fit(train_set_x, train_set_y)
-    print "%s" % kernel, clf.score(test_set_x, test_set_y)
+    print "SVC+rbf", clf.score(test_set_x, test_set_y)
     
     clf = sklearn.svm.LinearSVC()
     clf.fit(train_set_x, train_set_y)
