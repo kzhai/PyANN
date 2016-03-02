5dd4d95cb71422ae27cb76563dc9eaef858ec84f
Ke Zhai <kzhai@circlecar-lm.corp.yahoo.com>
2016-03-01 22:27:42 -0800

    update

--- PyMLP/plot_activations.py
+++ PyMLP/plot_activations.py
@@ -24,7 +24,7 @@ def parse_args():
     parser.set_defaults(# parameter set 1
                         input_directory=None,
                         model_directory=None,
-                        # batch_size=0,
+                        batch_size=0,
                         # best_model_only=False,
                         )
     # parameter set 1
@@ -33,8 +33,8 @@ def parse_args():
     parser.add_option("--model_directory", type="string", dest="model_directory",
                       help="output directory [None]");
 
-    # parser.add_option("--batch_size", type="int", dest="batch_size",
-                      # help="batch size [0]");
+    parser.add_option("--batch_size", type="int", dest="batch_size",
+                      help="batch size [0]");
     # parser.add_option("--best_model_only", action="store_true", dest="best_model_only",
                       # help="best model only");
 
@@ -62,11 +62,9 @@ def launch_test():
     # test_set_x = test_set_x / numpy.float32(256)
     test_set_y = numpy.load(os.path.join(input_directory, "test.label.npy"))
     
-    '''
     batch_size = options.batch_size;
     if batch_size <= 0:
         batch_size = test_set_x.shape[0];
-    '''
     
     '''
     print "========== ========== ========== ========== =========="
@@ -90,13 +88,23 @@ def launch_test():
     generalized_dropout_layers = [layer for layer in network.get_all_layers() if isinstance(layer, layers.dropout.GeneralizedDropoutLayer)];
     assert len(generalized_dropout_layers) == len(dense_layers)
     
+    indices = range(test_set_x.shape[0])
+    numpy.random.shuffle(indices);
+    test_subset_x = test_set_x[indices[:batch_size], :];
+    
     for layer_index in xrange(1, len(generalized_dropout_layers)):
-        test_layer_output = lasagne.layers.get_output(generalized_dropout_layers[layer_index], test_set_x, deterministic=True).eval();
-        
-        figure_path = os.path.join(model_directory, "%s-layer%d-output.pdf" % ("-".join(model_settings), layer_index))
+        test_layer_output = lasagne.layers.get_output(generalized_dropout_layers[layer_index], test_subset_x, deterministic=True).eval();
 
+        figure_path = os.path.join(model_directory, "%s-layer%d-output.pdf" % ("-".join(model_settings), layer_index))
+        
+        test_layer_output = numpy.reshape(test_layer_output, test_layer_output.shape[0] * test_layer_output.shape[1])
+        
+        min_value = numpy.min(test_layer_output);
+        max_value = numpy.max(test_layer_output);
+        
         # Make a normed histogram. It'll be multiplied by 100 later.
         n, bins, patches = matplotlib.pyplot.hist(test_layer_output, bins=100)
+        matplotlib.pyplot.xlim((min_value, max_value))
         matplotlib.pyplot.rc('font', size=30)
         # matplotlib.pyplot.xlabel("dropout rate")
     
